8fcf6b87f32f608e799b7b3e2d9411d6d467c981
Aniruddh Nagare <codebyanii@gmail.com>
2025-06-17 23:59:26 +0530

    Update BestSeller component layout for improved responsiveness
    - Adjusted grid layout in BestSeller component to enhance responsiveness across different screen sizes.
    - Changed column configuration to better accommodate product display on various devices.

--- client/src/Components/BestSeller.jsx
+++ client/src/Components/BestSeller.jsx
@@ -8,7 +8,7 @@ const BestSeller = () => {
     <div className="mt-16">
       <p className="text-2xl md:text-4xl font-medium text-start">Best Seller</p>
       <div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap 3 lg:grid-cols-5 mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 mt-6 justify-items-center">
           {
             products.filter((product) => product.inStock).slice(0, 5).map((product) => (
               <Card key={product._id} product={product} />
