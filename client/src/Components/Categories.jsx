0717f1dd25e61655a3ccdc695aa0181b57389d1f
Aniruddh Nagare <codebyanii@gmail.com>
2025-06-18 00:13:46 +0530

    Refactor Categories component grid layout for improved responsiveness
    - Adjusted grid configuration in Categories component to enhance layout across different screen sizes.
    - Removed redundant column setting for better clarity and performance.

--- client/src/Components/Categories.jsx
+++ client/src/Components/Categories.jsx
@@ -8,7 +8,7 @@ const Categories = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-4xl font-medium">Categories</p>
-      <div className="grid grid-cols-2 md:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6 mt-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-7 gap-6 mt-6">
         {categories.map((category, index) => (
           <div
             key={index}
